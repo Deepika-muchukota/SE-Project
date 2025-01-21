e3b7f231b37f1e7801cbea2fb786935abd5394ba
Deepika-muchukota <deepikamuchukota2001@gmail.com>
2025-01-20 23:56:12 -0500

    Update hello testing.md.m

--- hello testing.md.m
+++ hello testing.md.m
@@ -1,1 +1,7 @@
-hello testing
+Project Title: UF ShopEase
+
+Project Description: UF ShopEase is a smart and reliable e-commerce platform designed to make online shopping simple and efficient for businesses and customers. Built with React.js for an interactive front end and Golang for a fast, scalable back end, it ensures smooth performance and a user-friendly experience. Key features include seamless product browsing, secure transactions, real-time order tracking, and an intuitive admin dashboard for efficient management of inventory and sales.
+
+Contributors:
+Front End- Praneeth Buchepalli, Deepika Muchukota
+Back End- Kamal Kandula, Srinivas Kommineni
