11be9008971bc24434eec87f86ad22ddc2334ec6
buchepalli praneeth praneeth <buchepallipraneeth@gmail.com>
2025-02-28 22:22:06 -0500

    Added first level webpage for Baba's

---
+++ frontend/src/Baba/baba.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./baba_pizza.css"; 
+import veg from "./Baba_images/vegeterian_images.jpg";
+import non_veg from "./Baba_images/non-veg.jpg";
+
+const categories = [
+  { name: "Veg Options", image: veg },
+  { name: "Non-Veg Options", image:non_veg  },
+];
+
+function Baba() {
+  return (
+    <div className="baba-container">
+      <div className="overlay"></div> 
+      <div className="category-grid">
+        {categories.map((category, index) => (
+          <button key={index} className="category-button">
+            <img src={category.image} alt={category.name} className="category-image" />
+            <p className="category-name">{category.name}</p>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Baba;
