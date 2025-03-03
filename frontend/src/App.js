28fb8175bfe27d2a9f459954c2a1b60524381ba5
buchepalli praneeth praneeth <buchepallipraneeth@gmail.com>
2025-03-03 12:56:27 -0500

    Added Cart functionality for Sides

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -129,7 +129,7 @@ function App() {
           <Route path="/foodstalls/burger352/burger"  element={<Burger cart={cart} addItemToCart={addItemToCart} />} />
           <Route path="/foodstalls/burger352/chicken" element={<Chicken cart={cart} addItemToCart={addItemToCart} />} />
           <Route path="/foodstalls/burger352/shakes" element={<Shakes cart={cart} addItemToCart={addItemToCart} />} />
-          <Route path="/foodstalls/burger352/sides" element={<Sides />} />
+          <Route path="/foodstalls/burger352/sides" element={<Sides cart={cart} addItemToCart={addItemToCart} />} />
           <Route path="/foodstalls/burger352/steaks" element={<Steaks />} />
           <Route path="/foodstalls/Babas/ve" element={<Vegeterian />} />
           <Route path="/foodstalls/Babas/nve" element={<Nvegeterian />} />
--- frontend/src/Burger_352/Sides.js
+++ frontend/src/Burger_352/Sides.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./sides.css"; 
 import fries from "./burger352_images/fries.jpeg";
 import sweet_fries from "./burger352_images/sweet_fries.jpeg";
@@ -10,19 +10,40 @@ const categories = [
   { name: "onion_rings", image: onion_rings , price: "$3.09"}
 ];
 
-function Sides() {
+function Sides({ cart, addItemToCart }) {
+
+  const [selectedItems, setSelectedItems] = useState([]);
+    
+      useEffect(() => {
+        console.log("Chicken selection updated:", selectedItems);
+      }, [selectedItems]);
+    
+      const handleItemClick = (item) => {
+        if (selectedItems.some(i => i.name === item.name)) {
+          setSelectedItems(prev => prev.filter(i => i.name !== item.name));
+          addItemToCart(item, "remove");
+        } else {
+          setSelectedItems(prev => [...prev, item]);
+          addItemToCart(item, "add");
+        }
+      };
+    
   return (
     <div className="sides-opt-container">
       <div className="overlay"></div> 
       <div className="category-grid">
         {categories.map((category, index) => (
-          <button key={index} className="category-button">
+          <button
+            key={index}
+            className={`category-button ${cart.some(cartItem => cartItem.name === category.name) ? 'selected' : ''}`}
+            onClick={() => handleItemClick(category)}
+          >
             <img src={category.image} alt={category.name} className="category-image" />
             <p className="category-name">{category.name}<br/>{category.price}</p>
           </button>
         ))}
       </div>
-      <button className="confirm-order-btn">Confirm Order</button>
+      <button className="confirm-order-btn" onClick={() => console.log(cart)}>Confirm Order</button>
     </div>
   );
 }
