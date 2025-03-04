a213e7de559173b5659db11dfa209805fd6b13f7
srinu0602 <108626191+srinu0602@users.noreply.github.com>
2025-03-04 14:12:27 -0500

    updating main.go

---
+++ Back-End/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"SE-Project/Back-End/controllers"
+	"SE-Project/Back-End/database"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Connect to the database
+	database.ConnectDB()
+
+	// Create a new Gin router
+	r := gin.Default()
+
+	// Enable CORS
+	r.Use(cors.Default())
+
+	// Authentication Routes
+	r.POST("/api/users/signup", controllers.Signup)
+	r.POST("/api/users/signin", controllers.Signin)
+	r.GET("/api/users", controllers.GetUserByName)
+	r.PUT("/api/users/:id", controllers.EditUser)
+	r.DELETE("/api/users", controllers.DeleteUser)
+
+	// Food Stalls & Menu Routes
+	r.GET("/api/foodstalls", controllers.GetFoodStalls)
+	r.GET("/api/foodstalls/:id/menu", controllers.GetFoodMenu)
+
+	// Cart Routes
+	r.POST("/api/cart/add", controllers.AddItemToCart)
+	r.GET("/api/cart/:userId", controllers.FetchCartItems)
+	r.DELETE("/api/cart/delete/:id", controllers.DeleteItemFromCart)
+	r.DELETE("/api/cart/empty/:userId", controllers.EmptyCart)
+
+	// Start the server on port 5000
+	r.Run(":5000")
+}
