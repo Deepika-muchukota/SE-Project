3adf60a4d749baa104f933eee100195d3c1847af
Kamal Kandula <kamalkandulakk@gmail.com>
2025-03-28 20:14:11 -0400

    Added Name, Price, Quantity to the cart.go

--- Back-End/models/cart.go
+++ Back-End/models/cart.go
@@ -4,8 +4,10 @@ import "gorm.io/gorm"
 
 type CartItem struct {
 	gorm.Model
-	ID       uint `gorm:"primaryKey" json:"id"`
-	UserID   uint `gorm:"not null" json:"user_id"`
-	MenuID   uint `gorm:"not null" json:"menu_id"`
-	Quantity uint `gorm:"not null; check:quantity>0" json:"quantity"`
+	ID       uint    `gorm:"primaryKey" json:"id"`
+	UserID   uint    `gorm:"not null" json:"user_id"`    
+	MenuID   uint    `gorm:"not null" json:"menu_id"`    
+	Quantity uint    `gorm:"not null" json:"quantity"`
+	Name     string  `json:"name"`
+	Price    float64 `json:"price"`
 }
