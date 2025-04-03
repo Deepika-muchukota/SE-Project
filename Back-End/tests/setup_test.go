1a15467ab73e999cf00009c064590caad9776dcd
srinu0602 <108626191+srinu0602@users.noreply.github.com>
2025-04-03 14:29:39 -0400

    Create setup_test.go

---
+++ Back-End/tests/setup_test.go
@@ -0,0 +1,36 @@
+package tests
+
+import (
+	"SE-Project/Back-End/controllers"
+	"SE-Project/Back-End/database"
+	"github.com/gin-gonic/gin"
+)
+
+// SetupRouter initializes the router for testing
+func SetupRouter() *gin.Engine {
+	database.ConnectDB() // Ensure the database is connected
+
+	r := gin.Default()
+
+	// Define all API routes
+	r.POST("/api/users/signup", controllers.Signup)
+	r.POST("/api/users/signin", controllers.Signin)
+	r.GET("/api/users", controllers.GetUserByName)
+	r.PUT("/api/users/:id", controllers.EditUser)
+	r.DELETE("/api/users", controllers.DeleteUser)
+
+	r.GET("/api/foodstalls", controllers.GetFoodStalls)
+	r.GET("/api/foodstalls/:id/menu", controllers.GetFoodMenu)
+	r.POST("/api/cart/add", controllers.AddItemToCart)
+	r.GET("/api/cart/:userId", controllers.FetchCartItems)
+	r.DELETE("/api/cart/delete/:id", controllers.DeleteItemFromCart)
+	r.DELETE("/api/cart/empty", controllers.EmptyCart)
+
+	r.GET("/api/all-menu-items", controllers.GetAllMenuItems)
+	r.GET("/api/menu/item/:name", controllers.GetMenuItemByName)
+	r.PUT("/api/cart/update", controllers.UpdateCartItemQuantity)
+	r.POST("/api/users/forgot-password", controllers.ForgotPassword)
+	r.POST("/api/users/reset-password", controllers.ResetPassword)
+
+	return r
+}
