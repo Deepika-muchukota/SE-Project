0ab0a06f6a31401befd45565a8775ccdd6e534b3
Kamal Kandula <kamalkandulakk@gmail.com>
2025-04-20 20:40:50 -0400

    Update database.go

--- Back-End/database/database.go
+++ Back-End/database/database.go
@@ -11,14 +11,14 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "host=localhost user=postgres password=praneeth7073 dbname=ufshopease port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password=postgres dbname=ufshopease port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	// Migrate the schema
-	db.AutoMigrate(&models.User{}, &models.FoodStall{}, &models.MenuItem{}, &models.CartItem{})
+	db.AutoMigrate(&models.User{}, &models.FoodStall{}, &models.MenuItem{}, &models.CartItem{}, &models.Order{}, &models.OrderItem{})
 
 	DB = db
 	log.Println("Database connected successfully!!")
