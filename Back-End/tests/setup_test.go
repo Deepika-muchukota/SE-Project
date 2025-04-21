4779b32b8b1ae5a36e44d65c8ea692e2a49a2232
Kamal Kandula <kamalkandulakk@gmail.com>
2025-04-20 23:52:27 -0400

    Update setup_test.go

--- Back-End/tests/setup_test.go
+++ Back-End/tests/setup_test.go
@@ -13,24 +13,38 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	// Define all API routes
+	// Authentication Routes
 	r.POST("/api/users/signup", controllers.Signup)
 	r.POST("/api/users/signin", controllers.Signin)
 	r.GET("/api/users", controllers.GetUserByName)
 	r.PUT("/api/users/:id", controllers.EditUser)
-	r.DELETE("/api/users", controllers.DeleteUser)
+	r.POST("/api/users/delete/:id", controllers.DeleteUser)
+	r.PUT("/api/users/:id/change-password", controllers.ChangePassword)
+	r.POST("/api/users/logout", controllers.LogoutUserByEmail)
 
+	// Food Stalls & Menu Routes
 	r.GET("/api/foodstalls", controllers.GetFoodStalls)
 	r.GET("/api/foodstalls/:id/menu", controllers.GetFoodMenu)
-	r.POST("/api/cart/add", controllers.AddItemToCart)
-	r.GET("/api/cart/:userId", controllers.FetchCartItems)
-	r.DELETE("/api/cart/delete/:id", controllers.DeleteItemFromCart)
-	r.DELETE("/api/cart/empty", controllers.EmptyCart)
-
 	r.GET("/api/all-menu-items", controllers.GetAllMenuItems)
 	r.GET("/api/menu/item/:name", controllers.GetMenuItemByName)
-	r.PUT("/api/cart/update", controllers.UpdateCartItemQuantity)
-	r.POST("/api/users/forgot-password", controllers.ForgotPassword)
-	r.POST("/api/users/reset-password", controllers.ResetPassword)
+
+	// Cart Routes
+	r.POST("/api/cart/add", controllers.AddItemToCart)
+	r.GET("/api/cart/:userId", controllers.FetchCartItems)
+	r.DELETE("/api/cart/delete/:userId/:menuId", controllers.DeleteItemFromCart)
+	r.DELETE("/api/cart/empty/:userId", controllers.EmptyCart)
+
+	// Order Routes
+	r.GET("/api/orders/:userId/:orderId", controllers.GetSingleOrderDetails)
+	r.GET("/api/stats/user/:userId", controllers.GetUserOrderStats)
+	r.GET("/api/orders/:userId/latest", controllers.GetMostRecentOrder)
+	r.GET("/api/menu/popular-items", controllers.GetMostOrderedItems)
+
+	r.POST("/api/place-order/:userId", controllers.FinalizeOrder)
+	r.GET("/api/orders/:userId", controllers.GetOrdersByUser)
+
+	// Payment Routes
+	r.POST("/api/payments", controllers.HandlePayment)
 
 	return r
 }
